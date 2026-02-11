dd8e96568f4e186c4707fffc373758def22ca2a7
Shifin-Malik <shifinmalik46@gmail.com>
2026-02-11 12:39:56 +0530

    fix(cors): allow frontend domain for API access

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,10 +22,17 @@ const FRONTEND_URL = process.env.FRONTEND_URL;
 
 app.use(
   cors({
-    origin: FRONTEND_URL,
+    origin: [
+      "https://go-cart-three-murex.vercel.app",
+      "http://localhost:5173",
+      FRONTEND_URL
+    ],
     credentials: true,
-  }),
+  })
 );
+
+app.options("*", cors());
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
